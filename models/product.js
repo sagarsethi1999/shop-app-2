cec1eb18c68fec59cbeb276f8bc29777756d5aab
sagarsethi1999 <sagarsethiputulu@gmail.com>
2024-01-16 10:14:52 +0530

    editing and deleting

--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id,title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,11 +27,35 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
+      if(this.id)
+      {
+        const exisingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[exisingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      }
+
+      else
+      {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
+    });
+  }
+  
+  static deleteProductById(id) {
+    getProductsFromFile(products => {
+      const updatedProducts = products.filter(product => product.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (err) {
+          console.log('Error deleting product:', err);
+        }
       });
     });
   }
