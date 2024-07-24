003b4d17be9d70570eaa4c14a87ac128fd96c8e3
sagarsethi1999 <sagarsethiputulu@gmail.com>
2024-07-25 00:13:12 +0530

    deleting from cart using mongoose

--- models/user.js
+++ models/user.js
@@ -48,6 +48,14 @@ userSchema.methods.addToCart = function(product) {
   return this.save();
 };
 
+userSchema.methods.deleteCartItem = function(prodId){
+  const updatedCartItems = this.cart.items.filter( item => {
+          return item.productId.toString() !== prodId.toString();
+        });
+        this.cart.items = updatedCartItems;
+        return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
 
 
