f2399f1351ce984ac280a0b7998c3e4f43909069
sagarsethi1999 <sagarsethiputulu@gmail.com>
2024-07-25 00:06:09 +0530

    postcart and getcart with mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -56,8 +56,10 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
 
-  req.user.getCart()
-  .then(products => {
+  req.user
+  .populate('cart.items.productId')
+  .then(user => {
+     const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
